d0b882326de015591ed041adae6ce0efc48d0e10
Jakob Schoenicke <jakob.schoenicke@mailbox.org>
2021-11-17 17:17:51 +0100

    Added JWT token creation to /login/ and authentication to /user/**

--- src/main/java/ninja/parkverbot/restserver/security/LoginController.java
+++ src/main/java/ninja/parkverbot/restserver/security/LoginController.java
@@ -1,17 +1,50 @@
 package ninja.parkverbot.restserver.security;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
+// Based on https://www.techgeeknext.com/spring/spring-boot-security-token-authentication-jwt
 @RestController
 @RequestMapping("/login")
 public class LoginController {
 
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+
+    public LoginController(JwtTokenUtil jwtTokenUtil, AuthenticationManager authenticationManager, UserDetailsService userDetailsService) {
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+    }
+
     @PostMapping("/")
-    public void login() {
-        // ToDo Implement
+    public String login(@RequestBody JwtRequest authentificationRequest) throws Exception {
+        authenticate(authentificationRequest.getUsername(), authentificationRequest.getPassword());
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authentificationRequest.getUsername());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return token;
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        Objects.requireNonNull(username);
+        Objects.requireNonNull(password);
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
     }
 
 }
--- src/main/java/ninja/parkverbot/restserver/security/SecurityConfig.java
+++ src/main/java/ninja/parkverbot/restserver/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package ninja.parkverbot.restserver.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -16,20 +20,37 @@ public class SecurityConfig {
             http
                     .antMatcher("/login/")
                     .authorizeRequests(authorize -> authorize.anyRequest().authenticated())
-                    .csrf().disable() // Not for production
+                    .csrf().disable()// Not for production
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                     .httpBasic();
+        }
+
+        @Bean
+        @Override
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManagerBean();
         }
     }
 
     @Configuration
     @Order(2)
     public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+        private final JwtRequestFilter jwtRequestFilter;
+
+        public ApiWebSecurityConfigurationAdapter(JwtRequestFilter jwtRequestFilter) {
+            this.jwtRequestFilter = jwtRequestFilter;
+        }
+
         protected void configure(HttpSecurity http) throws Exception {
             http
                     .antMatcher("/user/**")
-                    .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
-                    .csrf().disable(); // Not for Production
+                    .authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                    .csrf().disable() // Not for Production
+                    .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         }
+
     }
 
     @Configuration
